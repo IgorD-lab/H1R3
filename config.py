d8ced60bf818b598c52329062b51e9e4a6ca1ae3
Igor Dobric <igordobricc@gmail.com>
2025-07-18 17:16:29 +0200

    using seperate config file for path

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-JOBS_JSON_PATH = os.path.join(DATA_DIR, "jobs_output.json")
 CV_PATH = os.path.join(DATA_DIR, "cv.txt")
-
-print (DATA_DIR)
+JOBS_OUTPUT_PATH = os.path.join(DATA_DIR, "jobs_output.json")
+AI_OUTPUT_PATH = os.path.join(DATA_DIR, "ai_output.json")
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from job_processing import runner
 from job_scraper import main_scraper
+import config
 import json
 
+
+# Path
+jobs_output_file = config.JOBS_OUTPUT_PATH
+ai_output_path = config.AI_OUTPUT_PATH
+cv_path = config.CV_PATH
+
+# Get text from CV
+try:
+    with open(cv_path, "r", encoding="utf-8") as f:
+        cv_text = f.read().strip()
+except FileNotFoundError:
+    cv_text = "I am a Python developer i can use C, C++, python i do data science i have 0 years of experience"
+    
+# Scrape job data
 scraped_data = main_scraper.main()
 
 # Output as JSON
-output_file = "./data/jobs_output.json"
-with open(output_file, "w", encoding="utf-8") as f:
+with open(jobs_output_file, "w", encoding="utf-8") as f:
     json.dump(scraped_data, f, ensure_ascii=False, indent=2)
-print(f"\nJob details written to {output_file}")
+print(f"\nJob details written to {jobs_output_file}")   
+print("Job scraping complete. Now you can run the AI evaluation on the scraped jobs.")
 
-proceed_AI = input("Data scraping complete and written to jobs_output.json.\nDo you wish to run jobs trough AI? (y/n): ").lower().strip()
+# Ask user if they want to proceed with AI evaluation
+proceed_AI = input("Do you wish to run jobs through AI? (y/n): ").lower().strip()
 if proceed_AI != "y":
     print("Goodbye")
 else:
-    cv_text = "I am a Python developer i can use C, C++, python i do data science i have 0 years of experience"
-    jobs_json_path = "./data/jobs_output.json"
-    output_path = "./data/match_results.json"
-    runner.main(cv_text, jobs_json_path, output_path)
+    runner.main(cv_text, jobs_output_file, ai_output_path)
 
 
 
